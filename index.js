da5350bd66f8364933473d89ab1f1296314f5cf0
JoltLaw <94019623+JoltLaw@users.noreply.github.com>
2022-10-18 10:02:13 -0600

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const header = document.getElementById("header");
-
-function toggleNav() {
-  if (!header.classList.contains("nav-open")) {
-    header.classList.add("nav-open");
-  } else {
-    header.classList.remove("nav-open");
-  }
-}
